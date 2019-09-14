6665445f8eebd24fbbc3e73958d2f024d7f40f2e
Darrian Soto <ddog350@gmail.com>
2019-09-14 15:07:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,4 @@ def introduction_to_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}
 end
 
-introduction_to_language("")
+introduction_to_language("Darrian", "Ruby") 
